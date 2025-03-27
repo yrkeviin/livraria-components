c2fb58359f696328c9d691fb6be06889a2dc0c50
yrkeviin <kevineziquiellima160@gmail.com>
2025-03-27 11:46:53 -0300

    Feat: Banner finish

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Page() {
     return (
-        <Header />
+        <>
+            <Header />
+            <Banner />
+        </>
     );
 }
--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,7 +2,7 @@ import styles from "../styles/Banner.module.css";
 
 export default function Banner() {
     return (
-        <section>
+        <section className={styles.banner}>
             <h1 className={styles.title} >Descubra mundos extraordinários!</h1>
 
             <p className={styles.descricao}>Promoção de lançamento com até 30% de desconto!</p>
