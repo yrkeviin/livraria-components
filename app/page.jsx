24d291d058dcbc2d6fc6f30a3fbdee5552d1858d
yrkeviin <kevineziquiellima160@gmail.com>
2025-03-27 09:58:57 -0300

    Feat: Base of header

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,7 @@
 import Header from "../components/Header";
+
+export default function Page() {
+    return (
+        <Header />
+    );
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,28 @@ import styles from "../styles/Header.module.css";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div className={styles.profile}>
-                <img src="" alt="" />
+            <div className={styles.title}>
+                <h1>BookVerse</h1>
+                <p>Onde histórias ganham vida</p>
+            </div>
+
+            <div>
+                <a className={styles.on} href="">Início</a>
+                <a href="">Livros</a>
+                <a href="">Categorias</a>
+                <a href="">Autores</a>
+                <a href="">Ofertas</a>
+            </div>
+
+            <div>
+                <input type="text" placeholder="Pesquisar" />
+                <button>Pesquisar</button>
+            </div>
+
+            <div>
+                <a href=""> <img src="" alt="" /> </a>
+                <a href=""> <img src="" alt="" /> </a>
+                <a href=""> <img src="" alt="" /> </a>
             </div>
         </header>
     );
