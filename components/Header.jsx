8a28c2a08878b4e1eda92df9b17df9f09db11bae
yrkeviin <kevineziquiellima160@gmail.com>
2025-03-27 09:45:31 -0300

    Feat: Reajust in files

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.profile}>
+                <img src="" alt="" />
+            </div>
+        </header>
+    );
+}
