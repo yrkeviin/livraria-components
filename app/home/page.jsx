1d331503bb96055440b015772454e59290963034
Ana Julia <anajuliademattei26@gmailcom>
2025-04-03 09:30:07 -0300

    add cards

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -14,10 +14,51 @@ export default function Page() {
                 text="Destaques da semana"
                 all="Ver todos"
             />
+
+            <Card 
+                        imgSrc="/livro01.png"
+                        title="Eu e Esse Meu Coração" 
+                        author="por C. C. Hunter" 
+                        avaliacao="⭐⭐⭐⭐  (4)" 
+                        genre="Romance" 
+                        price="R$ 29,90" 
+                        fav=" 💖  🛒"
+                    />
+            <Card 
+                        imgSrc="/livro02.png"
+                        title="A Culpa é das Estrelas" 
+                        author="por John Green" 
+                        avaliacao="⭐⭐⭐⭐⭐  (4.8)" 
+                        genre="Romance" 
+                        price="R$ 34,90"
+                        fav=" 💖  🛒" 
+                    />
+            <Card 
+                        imgSrc="/livro03.png"
+                        title="O lado feio do amor " 
+                        author="por Colleen Hoover" 
+                        avaliacao="⭐⭐⭐⭐  (4.7)"
+                        genre="Romance"  
+                        price="R$ 39,90" 
+                         fav=" 💖  🛒"
+                    />
+            <Card 
+                        imgSrc="/livro04.png"
+                        title="O Pequeno Príncipe" 
+                        author="por Antoine de Saint-Exupéry" 
+                        avaliacao="⭐⭐⭐⭐⭐  (4.9)" 
+                        genre="Infantil" 
+                        price="R$ 19,90"
+                        fav=" 💖  🛒" 
+                    />
+
             <Title
                 text="Categorias"
                 all="Ver todas"
             />
+
+
+
             <section className="categorias">
                 <CardCategoria 
                     emoji="🚀"
